caa3b64c8770a0148867ff3720f67cc4765b3132
gabriel-delima <gabriel.lima.moura@poli.ufrj.br>
2022-01-11 20:54:53 -0300

    reset de senha funcionando

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.extensions import db, migrate, ma, jwt, mail, cors
 
 from app.user.routes import user_api
 from app.auth.routes import auth_api
+from app.password_recovery.routes import pw_api
 def create_app():
 
     app = Flask(__name__)
@@ -18,5 +19,6 @@ def create_app():
 
     app.register_blueprint(user_api)
     app.register_blueprint(auth_api)
+    app.register_blueprint(pw_api)
 
     return app
