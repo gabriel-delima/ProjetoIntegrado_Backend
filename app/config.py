0952b1d52247cce39f8db0f2e444682d27d18408
gabriel-delima <gabriel.lima.moura@poli.ufrj.br>
2022-01-11 21:17:44 -0300

    configuracoes para o deploy

--- app/config.py
+++ app/config.py
@@ -1,15 +1,17 @@
-from app.sensive import Sensive as sensive
+from os import environ
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = sensive.SQLALCHEMY_DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = sensive.SQLALCHEMY_TRACK_MODIFICATIONS
-    JSON_SORT_KEYS = sensive.JSON_SORT_KEYS
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+    #SQLALCHEMY_DATABASE_URI = "postgresql://nkkewfncodswso:a6056423095bbc02133ab5aa8f69fa9d562d6d9fa5044767254d7e687ccc4a7c@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dei91gqsuri2du"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
 
-    MAIL_SERVER = sensive.MAIL_SERVER
-    MAIL_PORT = sensive.MAIL_PORT
-    MAIL_USERNAME = sensive.MAIL_USERNAME
-    MAIL_PASSWORD = sensive.MAIL_PASSWORD
-    MAIL_USE_TLS = sensive.MAIL_USE_TLS
-    MAIL_USE_SSL = sensive.MAIL_USE_SSL
+    MAIL_SERVER = environ.get('MAIL_SERVER')
+    MAIL_PORT = environ.get('MAIL_PORT')
+    MAIL_USERNAME = environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = environ.get('MAIL_PASSWORD')
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
 
-    JWT_SECRET_KEY = sensive.JWT_SECRET_KEY
+    SECRET_KEY = environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = environ.get('SECRET_KEY')
