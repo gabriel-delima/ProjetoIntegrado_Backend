044786b25e83f01f5c487c5e60a23eb53db3f0b2
gabriel-delima <gabriel.lima.moura@poli.ufrj.br>
2022-01-30 15:48:47 -0300

    correcao erro

--- app/sensor/schemas.py
+++ app/sensor/schemas.py
@@ -10,7 +10,7 @@ class SensorSchema( ma.SQLAlchemySchema):
 
     sensor_id = ma.Integer(dump_only=True)
     name = ma.String(required=True)
-    user_id = ma.Integer(dump_only=True)
+    user_id = ma.Integer(required=True)
 
     create_time = ma.DateTime(dump_only=True)
     update_time = ma.DateTime(dump_only=True)
