5875a74783c79624a92045eb8baad24de3046f41
gabriel-delima <gabriel.lima.moura@poli.ufrj.br>
2022-02-20 23:07:30 -0300

    retornar datas

--- app/graphs/controllers.py
+++ app/graphs/controllers.py
@@ -110,6 +110,7 @@ class MaxUvByDayGraph(MethodView):
                     if changed_index == True :
                         value_list.append(uv_max)
                         time_list.append(uv_max_datetime)
+                        print(uv_max_datetime.date)
                         uv_max = uv_now
                         uv_max_datetime = temp_day
                         changed_index = False
