971e3f6d1dfbff3fea8266c37057fe4a2345703d
gabriel-delima <gabriel.lima.moura@poli.ufrj.br>
2022-03-08 01:11:26 -0300

    reset db

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@
 from os import environ
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
-    #SQLALCHEMY_DATABASE_URI = "postgresql://nkkewfncodswso:a6056423095bbc02133ab5aa8f69fa9d562d6d9fa5044767254d7e687ccc4a7c@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dei91gqsuri2du"
+    #SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "postgresql://nkkewfncodswso:a6056423095bbc02133ab5aa8f69fa9d562d6d9fa5044767254d7e687ccc4a7c@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dei91gqsuri2du"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_SORT_KEYS = False
 
